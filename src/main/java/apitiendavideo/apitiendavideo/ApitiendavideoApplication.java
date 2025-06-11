198a835ba71cabccd99bc780bc5bb4dcf045d10c
davidcrvDev <juandanes401@gmail.com>
2025-06-11 13:11:28 -0500

    Modi cors y puerto en properties.

--- src/main/java/apitiendavideo/apitiendavideo/ApitiendavideoApplication.java
+++ src/main/java/apitiendavideo/apitiendavideo/ApitiendavideoApplication.java
@@ -94,7 +94,7 @@ public class ApitiendavideoApplication {
 		@Override
 		public void addCorsMappings(CorsRegistry registry) {
 			registry.addMapping("/**")
-					.allowedOrigins("https://tiendavideo-o8ux.onrender.com")
+					.allowedOrigins("*")
 					.allowedMethods("*")
 					.allowedHeaders("*");
 		}
